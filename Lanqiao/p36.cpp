30cbef2cec7152e8a9ee8dad6b494c84af371794
repiece <340508578@qq.com>
2025-12-28 16:42:34 +0800

    up

--- Lanqiao/p36.cpp
+++ Lanqiao/p36.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
-using namespace std;
+#include<bits/stdc++.h>
 
-int main() {
-    int x;
-    cout << "请输入一个数: ";
-    cin >> x;
-    cout << "你输入了: " << x << endl;
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    double a, b, c;
+    cin >>a>>b>>c;
+    if ((a+b>c)&&(a+c>b)&&(b+c>a))
+    {
+        double p = (a+b+c)/2;
+        double square = sqrt(p*(p-a)*(p-b)*(p-c));
+        printf("%.2f", square);
+    }else cout << "-1";
+
     return 0;
 }
