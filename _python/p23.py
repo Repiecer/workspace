d4ea3bccf850bf46bede6a9822666d877183c352
repiece <340508578@qq.com>
2026-06-24 23:07:23 +0800

    update

--- _python/p23.py
+++ _python/p23.py
@@ -1,4 +1,29 @@
-ls = 'abc'
-print(ls[1:])
-a b c d e
-0 1 2 3 4
+'''
+测试数据1:
+输入: mn, Ozsj
+输出: hi, June
+
+
+测试数据2:
+输入: udymts, N qtaj z
+输出: python, I love u
+'''
+def decoder(text):
+     result = []
+     for char in text:
+          if 'a' <= char <= 'z':
+               new_char = chr((ord(char) - ord('a') - 5) % 26 + ord('a'))
+               result.append(new_char)
+          elif 'A' <= char <= 'Z':
+               new_char = chr((ord(char) - ord('A') - 5) % 26 + ord('A'))
+               result.append(new_char)
+          else:
+               result.append(char)
+     return ''.join(result)
+def main():
+    text = input()
+    ans = decoder(text)
+    print(ans)
+
+
+main()
