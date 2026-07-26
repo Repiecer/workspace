e612e3a00b482020e0fe0b4f4bc3c31b8a0f0970
repiece <340508578@qq.com>
2026-07-26 15:21:35 +0800

    new

---
+++ _python/p25.py
@@ -0,0 +1,6 @@
+age = 23
+
+start = 3
+
+for x in range(start, age+2, 2):
+    print(x)
---
+++ smartmet/p1.py
@@ -0,0 +1,13 @@
+import numpy as np 
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr.shape)
+print(arr.ndim)
+print(arr.size)
+print(arr.dtype)
+zeros = np.zeros((3, 4))
+ones = np.ones((2, 3))
+print(zeros)
+print(ones)
+lin = np.linspace(0,1,4)
+print(lin)
+
